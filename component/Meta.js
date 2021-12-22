11833e1ac0532d4a72dbe72708cd6fffaad2a808
Khaled Kaddal <k-kaddal@khaleds-mbp.home>
2021-12-22 15:06:35 +0000

    fixing meta data

--- component/Meta.js
+++ component/Meta.js
@@ -11,16 +11,17 @@ const Meta = ({ title, keywords, description }) => {
             <meta name="keywords" content={keywords} />
             <meta name="description" content={description} />
             <meta charSet="utf-8" />
-            <link rel="icon" href="/favicon.ico" />
+            <link rel="icon" href="/favicon.png" />
             <title>{title}</title>
         </Head>
     )
 }
 
 Meta.defaultProps = {
-    title: 'Resense.Art',
-    keywords: 'Tech Art Gallery',
-    description: 'Tech Art Gallery',
+    title: 'Khaled Kaddal',
+    keywords: 'Artist',
+    description:
+        'Artist, Performer and Programmer | working with soundscapes, data, ai and computer vision as artistic medium.',
 }
 
 export default Meta
